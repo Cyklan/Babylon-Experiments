520b9c8d74a69de3864cf6e5caa5b8a9875b14a8
Nils Fahle <nils.fahle@marlamalde.group>
2022-08-18 22:47:29 +0200

    ITS ALIVE

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,61 @@
-import { Color4, Engine, Scene } from "@babylonjs/core";
+import { Color4, Engine, FreeCamera, HemisphericLight, Mesh, Scene, Vector3, WebGPUEngine } from "@babylonjs/core";
 
 export default class App {
-  canvas: HTMLCanvasElement;
-  engine: Engine;
-  scene: Scene;
+  canvas!: HTMLCanvasElement;
+  engine!: Engine;
+  scene!: Scene;
+  public renderer!: "webgpu" | "webgl";
+  sphere!: Mesh;
+  direction: "positive" | "negative" = "positive";
 
   constructor() {
+    this.init()
+  }
+
+  init = async () => {
     this.canvas = document.getElementById("app") as HTMLCanvasElement;
-    this.engine = new Engine(this.canvas, true);
+    if ("gpu" in navigator) {
+      // WEB GPU baybeeee
+      this.engine = new WebGPUEngine(this.canvas);
+      await (this.engine as WebGPUEngine).initAsync();
+      this.renderer = "webgpu";
+    } else {
+      this.engine = new Engine(this.canvas, true);
+      this.renderer = "webgl";
+    }
     this.scene = new Scene(this.engine);
     this.scene.clearColor = new Color4(0.1, 0.2, 0.3, 1.0);
 
-    this.main()
-  }
+    const camera = new FreeCamera("camera", new Vector3(0, 5, -10), this.scene);
+    camera.setTarget(Vector3.Zero());
 
-  main = () => {
+    camera.attachControl(this.canvas, true);
+
+    const light = new HemisphericLight("light", new Vector3(0, 1, 0), this.scene);
+    light.intensity = .7;
+
+    this.sphere = Mesh.CreateSphere("sphere", 16, 2, this.scene);
+
+    this.sphere.position.y = 2;
+
+    Mesh.CreateGround("ground", 6, 6, 2, this.scene);
+
+    document.getElementById("renderer-overlay")!.innerHTML += this.renderer;
+    this.main();
+  };
+
+  main = async () => {
+    const fpsOverlay = document.getElementById("fps")
+    const speed = 1 / 120
     this.engine.runRenderLoop(() => {
       this.scene.render();
+      fpsOverlay!.innerHTML = "FPS: " + this.engine.getFps().toFixed()
+      this.sphere.position.y += this.direction === "positive" ? speed : -speed;
+      if (this.sphere.position.y > 2) {
+        this.direction = "negative";
+      } else if (this.sphere.position.y < -2) {
+        this.direction = "positive";
+      }
     });
   };
 }
---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import VitePluginHtmlEnv from "vite-plugin-html-env";
+import { config } from "dotenv"
+config()
+
+/** @type {import('vite').UserConfig} */
+export default {
+  plugins: [
+    VitePluginHtmlEnv({
+      prefix: "{{",
+      suffix: "}}",
+      envPrefixes: [""]
+    })
+  ],
+  server: {
+    port: 3000
+  }
+}
